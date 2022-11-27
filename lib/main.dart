bfbb779fc448e15370e1c1ca1fb3ffdcec8b0ad6
volodya0 <volodauzuk02@gmail.com>
2022-11-27 17:00:39 +0200

    improved exam app: added edit fields for query, better layout

--- lib/main.dart
+++ lib/main.dart
@@ -19,19 +19,29 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.purple,
           ),
           home: const HomePage(),
         ));
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  Future<Map<String, dynamic>> fetchWeatherData([String? query]) async {
-    query ??= 'Lviv';
-    String url = '$APIurl/forecast?access_key=$APIkey&query=$query';
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isLoading = false;
+  List<String>? currentData;
+  String city = 'Lviv';
+
+  _HomePageState();
+
+  Future<Map<String, dynamic>>? fetchWeatherData() async {
+    String url = '$APIurl/forecast?access_key=$APIkey&query=$city';
 
     var response = await http.get(url);
     var result = json.decode(response.body);
@@ -42,53 +52,95 @@ class HomePage extends StatelessWidget {
   List<String> parseData(data) {
     List<String> result = [];
 
-    result.add('Location: ${data['location']['name']}');
+    result.add(
+        'Location: ${data['location']['name']}, ${data['location']['country']}');
     result.add('Time: ${data['location']['localtime']}');
-    result.add('Temperature: ${data['current']['temperature']}');
+    result.add(
+        'Temperature: ${data['current']['temperature']}, feels like ${data['current']['feelslike']}');
     result.add('Wind spreed: ${data['current']['wind_speed']}');
-    result.add('Wind spreed: ${data['current']['wind_speed']}');
+    result.add('Wind spreed: ${data['current']['visibility']}');
     result.add('Pressure: ${data['current']['pressure']}');
 
     return result;
   }
 
-  Future<List<String>> getData() async {
-    Map<String, dynamic> data = await fetchWeatherData();
+  void getData() async {
+    Map<String, dynamic>? data = await fetchWeatherData();
+
+    if (data == null || data['success'] == false) {
+      setState(() {
+        isLoading = false;
+      });
+      return;
+    }
+
     List<String> parsedData = parseData(data);
 
-    return parsedData;
+    setState(() {
+      currentData = parsedData;
+      isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather'),
-      ),
-      body: FutureBuilder<List<String>>(
-        future: getData(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    child: Row(
-                      children: [
-                        Text(
-                          snapshot.data[index],
-                          textAlign: TextAlign.left,
-                        )
-                      ],
-                    ),
-                  );
-                });
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Weather'),
+        ),
+        body: Column(children: [
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(children: [
+                SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        city = value;
+                      },
+                      initialValue: city,
+                      decoration: const InputDecoration(
+                        labelText: 'Location',
+                      ),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            currentData = null;
+                            isLoading = true;
+                            getData();
+                          });
+                        },
+                        child: const Text('Show weather')))
+              ])),
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: currentData != null
+                  ? ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(8),
+                      itemCount: currentData!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card(
+                          child: Row(
+                            children: [
+                              Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Text(
+                                    currentData![index],
+                                    textAlign: TextAlign.left,
+                                  ))
+                            ],
+                          ),
+                        );
+                      })
+                  : Center(
+                      child: isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text('The weather will appear here')))
+        ]));
   }
 }
