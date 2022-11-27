df739ae5dc33f14fcac5f6a3397ba9337393af48
volodya0 <volodauzuk02@gmail.com>
2022-11-27 13:57:14 +0200

    Initial version of exam app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:a/schedule.dart';
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
-import 'auth.dart';
-import 'home.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,16 +14,81 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Application',
-        theme: ThemeData(
-          primarySwatch: Colors.teal,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const AuthPage(),
-          '/home': (context) => const HomePage(),
-          '/schedule': (context) => const SchedulePage(),
-        });
+    return MediaQuery(
+        data: const MediaQueryData(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomePage(),
+        ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  Future<Map<String, dynamic>> fetchWeatherData([String? query]) async {
+    query ??= 'Lviv';
+    String url = '$APIurl/forecast?access_key=$APIkey&query=$query';
+
+    var response = await http.get(url);
+    var result = json.decode(response.body);
+
+    return result;
+  }
+
+  List<String> parseData(data) {
+    List<String> result = [];
+
+    result.add('Location: ${data['location']['name']}');
+    result.add('Time: ${data['location']['localtime']}');
+    result.add('Temperature: ${data['current']['temperature']}');
+    result.add('Wind spreed: ${data['current']['wind_speed']}');
+    result.add('Wind spreed: ${data['current']['wind_speed']}');
+    result.add('Pressure: ${data['current']['pressure']}');
+
+    return result;
+  }
+
+  Future<List<String>> getData() async {
+    Map<String, dynamic> data = await fetchWeatherData();
+    List<String> parsedData = parseData(data);
+
+    return parsedData;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather'),
+      ),
+      body: FutureBuilder<List<String>>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Text(
+                          snapshot.data[index],
+                          textAlign: TextAlign.left,
+                        )
+                      ],
+                    ),
+                  );
+                });
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
